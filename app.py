74546a82af085d9538e96d046cb929990cf5a41b
Abhishek yadav <74735261+abhicake2048@users.noreply.github.com>
2024-03-16 14:33:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,13 +36,8 @@ test = data.skip(450)
 model = mod.mod_create()
 
 # Training our model
-#tr_model = tr.comp(model,train,test)
+tr_model = tr.comp(model,train,test)
 
-# To download weights from a pretrained model
-#url = 'https://drive.google.com/uc?id=1vWscXs4Vt0a_1IH1-ct2TCgXAZT-N3_Y'
-#output = 'checkpoints.zip'
-#gdown.download(url, output, quiet=False)
-#gdown.extractall('checkpoints.zip', 'models')
 
 model.load_weights('models/checkpoint')
 
